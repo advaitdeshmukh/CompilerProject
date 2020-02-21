382ac8389745204549eae27f75c6d595c93cf5a4
advaitdeshmukh <advaitmaheshdeshmukh1@gmail.com>
2020-02-21 08:03:49 +0530

    efficency improvement in getgrammer and parsetable

--- getgrammar.c
+++ getgrammar.c
@@ -3,56 +3,59 @@
 #include <string.h>
 #define MAX_BUFFER_SIZE 1024
 
-char buffer[MAX_BUFFER_SIZE]; // the input buffer
-int bufferPosition; // indicates the current character to be read from the buffer
-int bufferSize = -1; // the number of characters stored in the buffer (-1 indicates first use)
-int isNT = 0;
 typedef struct unit {
     char* term;
     struct unit *next;
     int terminal;
 } unit;
 
-unit* array[104];
-char getnextcharacter(FILE *fp){
-	if(bufferSize == -1 || bufferPosition == bufferSize){
-        // first call to getCharacter or end of buffer has been reached
-        bufferSize = fread(buffer, sizeof(char), (size_t)MAX_BUFFER_SIZE, fp);
-        bufferPosition = 1;
-        if(bufferSize == 0)
-            return 26; // eof
-        else
-            return buffer[0];
-    }
+char buffer[MAX_BUFFER_SIZE]; // the input buffer
+int bufferPosition; // indicates the current character to be read from the buffer
+int bufferSize = -1; // the number of characters stored in the buffer (-1 indicates first use)
+int isNT = 0;
+int arraySize = 50;
+unit** array;
 
-    if(bufferSize == 0) // eof has been reached
-        return 26;
-    else
-        return buffer[bufferPosition++];
-}
-
-void addunit(unit *head, char* value1, int is){
-    if(head -> term == NULL){
-        head -> term = value1;
-        head-> terminal = is;
-        return;
-    }
-
-    unit *new = malloc(sizeof(unit)), *ptr = head;
-    new -> term = value1;
-    new -> terminal = is;
-    new -> next = NULL;
-    while (ptr -> next) ptr = ptr -> next;
-    ptr -> next = new;
-}
-
-void showlist (unit *head) {
+void showlist (unit *head){
     unit *ptr = head;
     while(ptr -> next){
         printf("%s %u \n", ptr -> term, ptr -> terminal);
         ptr = ptr -> next;
     }
     printf ("%s %u \n\n", ptr -> term, ptr -> terminal);
+}
+
+char getnextcharacter(FILE *fp){
+  if(bufferSize == -1 || bufferPosition == bufferSize){// first call to getCharacter or end of buffer has been reached
+      bufferSize = fread(buffer, sizeof(char), (size_t)MAX_BUFFER_SIZE, fp);
+      bufferPosition = 1;
+      if(bufferSize == 0)
+          return 26; // eof
+      else
+          return buffer[0];
+  }
+  if(bufferSize == 0) // eof has been reached
+      return 26;
+  else
+      return buffer[bufferPosition++];
+}
+
+void addunit(unit *head, char* value1, int is){
+  if(head -> term == NULL){
+      head -> term = value1;
+      head-> terminal = is;
+      return;
+  }
+  unit *new = malloc(sizeof(unit));
+  unit *ptr = head;
+  new -> term = value1;
+  new -> terminal = is;
+  new -> next = NULL;
+  showlist(head);
+  while (ptr -> next){
+    ptr = ptr -> next;
+  }
+  ptr -> next = new;
 }
 
 char* getnextterm(FILE *fp){
@@ -105,31 +108,43 @@ char* getnextterm(FILE *fp){
 void getnextrule(FILE *fp, unit* head){
   char next;
 	do {
-    //buffer--;
     char* nextterm = getnextterm(fp);
     if (nextterm != NULL){
       addunit(head, nextterm, isNT);
     }
-
     bufferPosition--;
     next = getnextcharacter(fp);
-		//showrule();
 	} while( next != '\n' && next != 26);
+  showlist(head);
+}
+
+void initializearray(){
+  array = (unit **)malloc(arraySize*sizeof(unit));
+  for(int i = 0; i < arraySize; i++){
+    array[i] = malloc(sizeof(unit));
+  }
+}
+
+void resizearray(){
+  arraySize = arraySize + 10;
+  array = (unit**)realloc(array,arraySize*sizeof(unit));
+  for(int i = arraySize - 10; i < arraySize; i++){
+    array[i] = calloc(1,sizeof(unit));
+  }
 }
 
 int main(){
 	  FILE* fp = fopen("grammar.txt", "r");
+    initializearray();
     char next;
-    for(int i = 0; i < 104; i++){
-      array[i] = malloc(sizeof(unit));
-    }
-
     int index = 0;
     do {
+      if(index == arraySize){
+        resizearray();
+      }
       getnextrule(fp, array[index]);
-      printf("line no : %u\n", index+1);
-      showlist(array[index]);
       next = getnextcharacter(fp);
+      //showlist(array[index]);
       index++;
   	} while( next != 26 && index < 104);
 }
